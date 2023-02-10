e9a93d5b4a8b350cccd4547f1794a862f8bdeaf8
sh-mithu <87830860+sh-mithu@users.noreply.github.com>
2023-02-10 23:38:27 +0600

    Add files via upload

---
+++ A._Sum_it_up.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int a,b,c,sum=0;
+    cin>>a>>b>>c;
+    sum=a+b+c;
+    cout<<"Sum of "<<a<<", "<<b<<" and "<<c<<" is "<<sum<<endl;
+    return 0;
+}
+
