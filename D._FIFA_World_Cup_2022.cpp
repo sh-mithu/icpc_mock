53d79d696364780e0eb68a4c02cc88a3750fb7b0
sh-mithu <87830860+sh-mithu@users.noreply.github.com>
2023-02-11 00:46:05 +0600

    Add files via upload

---
+++ D._FIFA_World_Cup_2022.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+
+//    cout<<"Argentina"<<endl;
+//    string winner="Argentina";
+//    for(int i=0;i<winner.size();i++){
+//        cout<<"Argentina"<<endl;
+//    }
+    cout<<"France"<<endl;
+    return 0;
+}
+
